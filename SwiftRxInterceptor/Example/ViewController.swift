d4f360712efc2b1263a70c8ae697b49846b4f073
jp.alary <alary.jeanpierre@gmail.com>
2016-11-26 00:29:00 +0100

    Prepared project
    - Example and Source folders
    - No storyboard and Asset catalog
    - Minimum support to iOS 8.0

---
+++ SwiftRxInterceptor/Example/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SwiftRxInterceptor
+//
+//  Created by Jean-Pierre Alary on 26/11/2016.
+//  Copyright © 2016 Jean-Pierre Alary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
